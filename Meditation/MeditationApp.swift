7266bf2ecc3c7081d23a124a9f3c9920155fd8e6
imhalid <imhalid@icloud.com>
2022-02-02 23:15:39 +0300

    Initial Commit

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Halid İçli on 2.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
